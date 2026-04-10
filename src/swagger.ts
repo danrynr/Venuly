336a649b0d0c0fb08495ed47d7b0c2ad855ccdb0
danrynr <daniel.reynard.k@gmail.com>
2026-04-10 17:14:26 +0700

    fix: api 4xx error

--- src/swagger.ts
+++ src/swagger.ts
@@ -2,5 +2,5 @@ import fs from "fs";
 import path from "path";
 import yaml from "js-yaml";
 
-const swaggerFile = path.join(__dirname, "..", "swagger.yaml");
+const swaggerFile = path.join(process.cwd(), "swagger.yaml");
 export const swaggerSpec = yaml.load(fs.readFileSync(swaggerFile, "utf8")) as object;
